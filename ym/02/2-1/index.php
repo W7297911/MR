1beaab70a9c76150045463fbd0b7e924cd573980
W7297911 <W7297911@163.COM>
2016-08-28 17:45:05 +0800

    添加文件

---
+++ ym/02/2-1/index.php
@@ -0,0 +1,23 @@
+<!
+* Created by PhpStorm.
+* User: wangyafei
+* Date: 2016/8/28
+* Time: 17:12
+>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <title>布尔型</title>
+</head>
+<body>
+<?php
+
+$b = true;
+if($b == true)
+    echo "<font color='aqua'>我们要努力学习PHP</font>";
+else
+    echo "<font color='red'>出错！";
+?>
+</body>
+</html>
---
+++ ym/02/2-2/index.php
@@ -0,0 +1,22 @@
+<!
+* Created by PhpStorm.
+* User: wangyafei
+* Date: 2016/8/28
+* Time: 17:12
+>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>字符串型</title>
+</head>
+<body>
+<?php
+
+$a = "你好，欢迎来到PHP的世界！";
+echo "<h3>$a<h3>";
+echo '<h3>$a<h3>';
+?>
+
+</body>
+</html>
