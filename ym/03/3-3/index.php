81517be969431ad7d790e21aee020b100f9b1bf2
W7297911 <W7297911@163.COM>
2016-09-02 00:51:38 +0800

    增加第三节文件

---
+++ ym/03/3-3/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/9/2
+ * Time: 0:37
+ */
+$n_name = 100;
+$str1 = false;
+$str2 = "";
+echo "$n_name";
+echo "$str1";
+echo "$str2";
+?>
+
