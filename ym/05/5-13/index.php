53dff6afb7f036011d74fca63e1950326777b1c7
wang'ya'fei <w297911@163.com>
2016-09-23 19:36:14 +0800

    增加文件

---
+++ ym/05/5-13/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: feiers
+ * Date: 2016/9/22
+ * Time: 18:55
+ */
+
+$str = "PHP自学教程@ASP.NET自学教程@ASP自学教程@JSP自学教程";
+$str_arr = explode("@",$str);
+print_r($str_arr);
+
---
+++ ym/05/5-14/5-14/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: feiers
+ * Date: 2016/9/22
+ * Time: 18:55
+ */
+
+
+
