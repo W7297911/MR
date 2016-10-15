d86f3c5e52a474754c2bd6a021897628281a7504
wang'ya'fei <w297911@163.com>
2016-10-15 10:31:05 +0800

    增加index1.php

---
+++ public/index1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: feiers
+ * Date: 2016/10/15
+ * Time: 10:26
+ */
---
+++ public/post.php
@@ -0,0 +1,15 @@
+<table width="501" border="0" align="center" cellspacing="1" bgcolor="#BBBBBB">
+<tr>
+  <td height="43" colspan="2" bgcolor="#DDDDDD">
+    <font color="#333333" size="+2">
+      您输入的个人资料信息
+    </font>
+  </td>
+</tr>
+<tr>
+  <td width="104" height="20" align="right" bgcolor="#DDDDDD">
+    姓名：
+  </td>
+</tr>
+
+</table>
