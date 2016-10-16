84f998f2b8f756dd2e0b05e3d00dc1454e19bc3e
W7297911 <W7297911@163.COM>
2016-10-16 20:21:14 +0800

    增加四个文件

--- public/index.php
+++ public/index.php
@@ -1,3 +1,109 @@
-<?php
-echo "PhpStorm";
-?>>
+<form id="form1" name="form1" method="post" action="post.php">
+  <table  width="503" border="0" align="center" cellspacing="1" bgcolor="#BBBBBB">
+    <tr>
+      <td height="46" colspan="2" bgcolor="#DDDDDD">
+        <font color="#333333" size="+2">
+          请输入你的个人信息
+        </font>
+      </td>
+    </tr>
+    <tr>
+      <td width="82" height="20" align="right" bgcolor="#DDDDDD">
+        姓名：
+      </td>
+      <td width="414" height="20" bgcolor="#DDDDDD">
+        <input type="text" name="name"/>
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+        性别：
+      </td>
+      <td height="20" bgcolor="#DDDDDD">
+        <input type="radio" name="sex" value="男"/>男
+        &nbsp;&nbsp;<input type="radio" name="sex" value="女"/>女
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+        生日：
+      </td>
+      <td height="20" bgcolor="#DDDDDD">
+        <select name="year">
+          <?php
+          for ($i=1900; $i <=2010 ; $i++) {
+            # 循环输出年份
+            echo "<option value='".$i."'".($i==1988?"selected":"").">".$i."年</option>";
+          }
+          ?>
+        </select>
+        <select name="month">
+          <?php
+          for ($i=1; $i <=12 ; $i++) {
+            #循环输出月份
+            echo "<option value='".$i."'".($i==1?"selected":"").">".$i."月</option>";
+          }
+           ?>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+        爱好：
+      </td>
+      <td height="20"  bgcolor="#DDDDDD">
+        <input type="checkbox" name="interest[]" value="看电影"/>
+        看电影
+        <input type="checkbox" name="interest[]" value="听音乐"/>
+        听音乐
+        <input type="checkbox" name="interest[]" value="演奏乐器"/>
+        演奏乐器
+        <input type="checkbox" name="interest[]" value="打篮球"/>
+        打篮球
+        <input type="checkbox" name="interest[]" value="看书"/>
+        看书
+        <input type="checkbox" name="interest[]" value="上网"/>
+        上网
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+        地址：
+      </td>
+      <td height="20"  bgcolor="#DDDDDD">
+        <input type="text" name="address"/>
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+        电话：
+      </td>
+      <td height="20"  bgcolor="#DDDDDD">
+        <input type="text" name="tel"/>
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+          qq：
+      </td>
+      <td height="20"  bgcolor="#DDDDDD">
+        <input type="text" name="qq"/>
+      </td>
+    </tr>
+    <tr>
+      <td height="20" align="right" bgcolor="#DDDDDD">
+          自我评价：
+      </td>
+      <td bgcolor="#DDDDDD"><textarea name="comment" rows="5" cols="30"></textarea>
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#DDDDDD">&nbsp;
+      </td>
+      <td bgcolor="#DDDDDD">
+        <input type="submit" name="Submit" value="提交"/>
+        <input type="reset" name="Submit2" value="重置"/>
+      </td>
+    </tr>
+  </table>
+</form>
--- public/index1.php
+++ public/index1.php
@@ -1,7 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: feiers
- * Date: 2016/10/15
- * Time: 10:26
- */
+<form name="myform" method="post">
+    <input name="interest[]" type="checkbox" value="sports"/>体育
+    <input name="interest[]" type="checkbox" value="music"/>音乐
+    <input name="interest[]" type="checkbox" value="film"/>影视<br/>
+    <input name="pic[]" type="file"/><br/>
+    <input name="pic[]" type="file"/><br/>
+    <input name="pic[]" type="file"/>
+    
+</form>
